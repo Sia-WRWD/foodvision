c21efc8439c7b78e550cf6729b24901f06d956f5
Sia-WRWD <chinojen7@gmail.com>
2023-07-08 00:03:41 +0800

    Added allergy symptoms

--- src/app/pages/information/allergy-information/allergy-information.component.ts
+++ src/app/pages/information/allergy-information/allergy-information.component.ts
@@ -7,9 +7,40 @@ import { Component, Input } from '@angular/core';
 })
 export class AllergyInformationComponent {
   @Input() allergens!: string[];
-  test: any = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  symptoms: any =
+    [
+      { name: "Eczema", img: "../../../../assets/symptom-img/eczema.jpg", description: "Dry, itchy, inflamed skin.", threatLevel: "Low" },
+      { name: "Abdominal Pain", img: "../../../../assets/symptom-img/abdominal-pain.jpg", description: "Pain or discomfort in the abdominal area.", threatLevel: "Low" },
+      { name: "Swelling Face", img: "../../../../assets/symptom-img/swollen-face.jpg", description: "Swelling or puffiness of the face.", threatLevel: "Moderate" },
+      { name: "Swelling Tongue", img: "../../../../assets/symptom-img/swollen-tongue.jpg", description: "Swelling of the tongue.", threatLevel: "High" },
+      { name: "Swelling Lips", img: "../../../../assets/symptom-img/swollen-lips.jpg", description: "Swelling and itching sensation of the lips.", threatLevel: "High" },
+      { name: "Vomitting", img: "../../../../assets/symptom-img/vomitting.jpg", description: "Feeling of sickness or discomfort in the stomach.", threatLevel: "Low" },
+      { name: "Headache", img: "../../../../assets/symptom-img/headache.jpg", description: "Pain or discomfort in the head.", threatLevel: "Low" },
+      { name: "Trouble Breathing", img: "../../../../assets/symptom-img/trouble-breathing.jpg", description: "Difficulty breathing or shortness of breath.", threatLevel: "High" },
+      { name: "Diarrhea", img: "../../../../assets/symptom-img/diarrhea.jpg", description: "Frequent and watery bowel movements.", threatLevel: "Low" }
+    ]
+
+  selectedSymptom: any;
+  treatments: any =
+    [
+      { name: "Adrenaline Injection", img: "", description: "" },
+      { name: "Antihistamines", img: "", description: "" },
+      { name: "Corticosteroids", img: "", description: "" },
+      { name: "Supportive Care", img: "", description: "" }
+    ];
+  selectedTreatment: any;
+
 
   ngOnInit() {
-    console.log(this.allergens);
+    this.selectedSymptom = this.symptoms[0];
+    this.selectedTreatment = this.treatments[0];
+  }
+
+  displaySymptom(symptomNo: number) {
+    this.selectedSymptom = this.symptoms[symptomNo];
+  }
+
+  displayTreatment(treatmentNo: number) {
+    this.selectedTreatment = this.treatments[treatmentNo];
   }
 }
