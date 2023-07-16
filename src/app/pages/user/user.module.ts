45229b652ae3001aab12bedf87c38035659f0e7c
Sia-WRWD <chinojen7@gmail.com>
2023-07-16 14:01:29 +0800

    removed history and bookmark components

--- src/app/pages/user/user.module.ts
+++ src/app/pages/user/user.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile/profile.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
-import { BookmarkComponent } from './bookmark/bookmark.component';
-import { HistoryComponent } from './history/history.component';
 import { AMModule } from 'src/app/am.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
@@ -17,8 +15,6 @@ import { foodNamePipe } from '../shared/validators/name.pipe';
     ProfileComponent,
     RegisterComponent,
     LoginComponent,
-    BookmarkComponent,
-    HistoryComponent,
     ForgotPasswordComponent,
     foodNamePipe
   ],
@@ -31,9 +27,7 @@ import { foodNamePipe } from '../shared/validators/name.pipe';
   exports: [
     ProfileComponent,
     RegisterComponent,
-    LoginComponent,
-    BookmarkComponent,
-    HistoryComponent
+    LoginComponent
   ]
 })
 export class UserModule { }
